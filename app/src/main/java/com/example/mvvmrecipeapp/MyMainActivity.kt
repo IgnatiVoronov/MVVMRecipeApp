9c3b35c30b8a54cf13c0e93c2f510712c3eb9f9a
Ignati <gntvrnv@gmail.com>
2023-12-20 11:04:06 +0300

    Using of fragment and custom view with Jetpack Compose

---
+++ app/src/main/java/com/example/mvvmrecipeapp/MyMainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mvvmrecipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView((R.layout.activity_my_main))
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container,RecipeListFragment())
+            .commit()
+    }
+}
