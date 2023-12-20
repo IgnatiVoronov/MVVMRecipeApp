298e6c27850c17b25e1d9df7fbb9fd4326bd12c0
Ignati <gntvrnv@gmail.com>
2023-12-20 11:09:29 +0300

    Using of fragment and custom view with Jetpack Compose

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.mvvmrecipeapp
 
-import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.fragment.app.Fragment
 
 class MainActivity : ComponentActivity() {
 }
